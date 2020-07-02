1a3a5c4c506f741209e32421b47c00959da1a256
Misbah-Swaleheen <misbahSwalheen@yahoo.com>
2020-07-02 23:18:43 +0500

    website

---
+++ app2.js
@@ -0,0 +1,20 @@
+$(".owl-carousel").owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    items: 4,
+    nav: true,
+    dots: false,
+    loop: true,
+    margin: 10,
+   responsive: {
+       0:{
+           items: 1
+       },
+       600:{
+           items: 3
+       },
+       1000:{
+           items: 4
+       }
+   }
+});
